4d8ffc4e8838452e114dcc425e60312958132086
Gerardo Calderón <g3rcar@gmail.com>
2015-04-20 23:01:57 -0600

    Traducido

--- chat-server.js
+++ chat-server.js
@@ -39,7 +39,7 @@ var server = http.createServer(function(request, response) {
     // Not important for us. We're writing WebSocket server, not HTTP server
 });
 server.listen(webSocketsServerPort, function() {
-    console.log((new Date()) + " Server is listening on port " + webSocketsServerPort);
+    console.log((new Date()) + " Server escuchando el puerto " + webSocketsServerPort);
 });
  
 /**
@@ -54,7 +54,7 @@ var wsServer = new webSocketServer({
 // This callback function is called every time someone
 // tries to connect to the WebSocket server
 wsServer.on('request', function(request) {
-    console.log((new Date()) + ' Connection from origin ' + request.origin + '.');
+    console.log((new Date()) + ' Conexión desde el origin ' + request.origin + '.');
  
     // accept connection - you should check 'request.origin' to make sure that
     // client is connecting from your website
@@ -65,7 +65,7 @@ wsServer.on('request', function(request) {
     var userName = false;
     var userColor = false;
  
-    console.log((new Date()) + ' Connection accepted.');
+    console.log((new Date()) + ' Conexión aceptada.');
  
     // send back chat history
     if (history.length > 0) {
@@ -81,11 +81,11 @@ wsServer.on('request', function(request) {
                 // get random color and send it back to the user
                 userColor = colors.shift();
                 connection.sendUTF(JSON.stringify({ type:'color', data: userColor }));
-                console.log((new Date()) + ' User is known as: ' + userName
-                            + ' with ' + userColor + ' color.');
+                console.log((new Date()) + ' Usuario conocido como: ' + userName
+                            + ' con ' + userColor + ' color.');
  
             } else { // log and broadcast the message
-                console.log((new Date()) + ' Received Message from '
+                console.log((new Date()) + ' Mensaje recibido de '
                             + userName + ': ' + message.utf8Data);
                 
                 // we want to keep history of all sent messages
@@ -111,7 +111,7 @@ wsServer.on('request', function(request) {
     connection.on('close', function(connection) {
         if (userName !== false && userColor !== false) {
             console.log((new Date()) + " Peer "
-                + connection.remoteAddress + " disconnected.");
+                + connection.remoteAddress + " desconectado.");
             // remove user from the list of connected clients
             clients.splice(index, 1);
             // push back user's color to be reused by another user
